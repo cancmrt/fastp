f9180a7104db86ae1081b742471b6aadde287a46
ccomert <can.comert@mfa.gov.tr>
2024-02-09 16:16:54 +0300

    fastp son değişiklikler

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var cors = require('cors')
+var cors = require('cors');
+var shell = require('shelljs');
 var StickyMiddleware = require('./fastp-core/fastp-sticky-middleware');
 const config = require('config');
 
@@ -7,18 +8,23 @@ if(!config.has("type")){
     console.log("Config dosyası okunamadı");
     throw "Config dosyası okunamadı";
 }
+try{
+  shell.exec('npx kill-port 3124 -y');
+  var app = express();
 
-var app = express();
+  app.use(cors());
 
-app.use(cors());
+  var sm = new StickyMiddleware(app);
+  app = sm.StickIt(app);
 
-var sm = new StickyMiddleware(app);
-app = sm.StickIt(app);
+  app.get('/', function (req, res) {
+    res.send('Hello From Fastp!');
+  });
 
-app.get('/', function (req, res) {
-  res.send('Hello From Fastp!');
-});
-
-app.listen(3124, function () {
-  console.log('Example app listening on port 3124!');
-});
+  app.listen(3124, function () {
+    console.log('Example app listening on port 3124!');
+  });
+}
+catch{
+  
+}
